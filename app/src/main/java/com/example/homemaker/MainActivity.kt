78297425e064bd407e1b9a704439abf02736f134
KyleCole01 <kyle@misfitselectronics.com>
2019-09-21 15:43:15 -0400

    add an intent to launch the recipe list

--- app/src/main/java/com/example/homemaker/MainActivity.kt
+++ app/src/main/java/com/example/homemaker/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.homemaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        launch_recipe_list_button.setOnClickListener {
+            startActivity(Intent(this,RecipeListActivity::class.java))
+        }
     }
 }
