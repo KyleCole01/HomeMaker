6935215f34156d8057d8f855ec9590404d48003c
KyleCole01 <kyle@misfitselectronics.com>
2019-09-21 16:01:37 -0400

    create a recyclerview layout for the recipelist

--- app/src/main/java/com/example/homemaker/Recipe.kt
+++
@@ -1,6 +0,0 @@
-package com.example.homemaker
-
-import android.media.Image
-
-
-class Recipe ( var title:String, var ingredientList:String, var directions:String,var category:String, var image:Image)
