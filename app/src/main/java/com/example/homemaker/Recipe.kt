d55947fac31aa5f1a9f709274679716544f1f9cb
KyleCole01 <kyle@misfitselectronics.com>
2019-09-21 15:31:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/homemaker/Recipe.kt
@@ -0,0 +1,6 @@
+package com.example.homemaker
+
+import android.media.Image
+
+
+class Recipe ( var title:String, var ingredientList:String, var directions:String,var category:String, var image:Image)
